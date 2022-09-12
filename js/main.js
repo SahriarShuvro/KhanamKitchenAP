78214c73c72859ae5eb99da48071ab622d92c401
SahriarShuvro <80584229+SahriarShuvro@users.noreply.github.com>
2022-09-13 02:08:57 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,81 @@
+// admin toggle 
+{
+    let singTogl = document.getElementById('sing-in');
+    let adDrop = document.getElementById('adDrop');
+
+    singTogl.addEventListener('click', function() {
+        adDrop.classList.toggle('adminMenu');
+    })
+}
+
+
+// menu bar toggle
+{
+    let menuBarT = document.getElementById('d-menu');
+    let menuBarActive = document.getElementById('menuBarActive');
+    let menuB = document.getElementById('menuB');
+
+    menuBarT.addEventListener('click', function() {
+        menuBarActive.classList.toggle('menuToglActive');
+        menuB.classList.toggle('FmenuToglActive');
+    })
+}
+
+// {
+//     window.onload = () => {
+//         const tab_switchers = document.querySelectorAll('[data-switcher]');
+
+//         for (let i = 0; i < tab_switchers.length; i++) {
+//             const tab_switcher = tab_switchers[i];
+//             const page_id = tab_switcher.dataset.tab;
+
+//             tab_switcher.addEventListener('click', () => {
+//                 document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
+//                 tab_switcher.parentNode.classList.add('is-active');
+
+//                 SwitchPage(page_id);
+//             });
+//         }
+//     }
+
+//     function SwitchPage(page_id) {
+//         console.log(page_id);
+
+//         const current_page = document.querySelector('.pages .page.is-active');
+//         current_page.classList.remove('is-active');
+
+//         const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+//         next_page.classList.add('is-active');
+//     }
+// }
+
+// Hamburger Toggle 
+{
+    let hamburger = document.getElementById('hamburger');
+    let menuBarHide = document.getElementById('menubar');
+
+    hamburger.addEventListener('click', function() {
+        menuBarHide.classList.toggle('activeHam');
+    })
+}
+
+// Image S
+{
+    function showPreview(event) {
+        if (event.target.files.length > 0) {
+            var src = URL.createObjectURL(event.target.files[0]);
+            var preview = document.getElementById("file-ip-1-preview");
+            preview.src = src;
+            preview.style.display = "block";
+        }
+    }
+} {
+    function showPreview2(event) {
+        if (event.target.files.length > 0) {
+            var src = URL.createObjectURL(event.target.files[0]);
+            var preview = document.getElementById("file-ip-2-preview");
+            preview.src = src;
+            preview.style.display = "block";
+        }
+    }
+}
